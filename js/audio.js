e41f79531b12dd1072aceac9ae8648c7fdf20021
Kooyer <36271069+Kooyer@users.noreply.github.com>
2018-02-12 23:46:34 +0800

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,6 @@
-/*
+
+				
+	/*
 	 * bug  说明  chrome下不能设置currentTime 不能完成快进 
 	 * */
 	
@@ -12,7 +14,7 @@
 		 musicList:[
 			{songSheet:"我的歌单",name:"等你下课",src:"music/周杰伦-等你下课.mp3",author:"周杰伦"},
 			{songSheet:"我的歌单",name:"刚好遇见你",src:"music/刚好遇见你.mp3",author:"李玉刚"},
-			 {songSheet:"我的歌单",name:"前世情人",src:"music/前世情人.mp3",author:"周杰伦"},
+		        {songSheet:"我的歌单",name:"前世情人",src:"music/前世情人.mp3",author:"周杰伦"},
 		]
 		 
 	}
@@ -527,4 +529,4 @@
 		window.getComputedStyle(obj,null)[attr];
 		if(v=="auto")v = 0;
 		return v;
-	};
+	};			
