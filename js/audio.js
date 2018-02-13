b56f20498616d38e3a5948f9391c6e3ccb18f803
Kooyer <36271069+Kooyer@users.noreply.github.com>
2018-02-13 12:26:43 +0800

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -21,6 +21,7 @@
 			 {songSheet:"我的歌单",name:"晴天",src:"music/晴天.mp3",author:"周杰伦"},
 			{songSheet:"我的歌单",name:"算什么男人",src:"music/算什么男人.mp3",author:"周杰伦"},
 			{songSheet:"我的歌单",name:"给我一首歌的时间",src:"music/给我一首歌的时间.mp3",author:"周杰伦"},
+			 {songSheet:"我的歌单",name:"简单爱（live）",src:"music/简单爱（live）.mp3",author:"周杰伦、陈奕迅"},
 			 
 		]
 		 
